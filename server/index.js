dc40f2e9b9709cddc9e59a03d9a09689f5a3c297
Jason <jason.ou@nyu.edu>
2019-04-14 14:46:26 -0400

    initial-commit

---
+++ server/index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+import { Client, environments } from "plaid";
+
+const vars = {
+  PLAID_CLIENT_ID: "5c52345ce341ed0010a522f1",
+  PLAID_SECRET: "259a3db7aec2d3314a6e545d056a10",
+  PLAID_PUBLIC_KEY: "134893e5d974bced3a52c91e8e6b5a",
+  PLAID_ENV: "development"
+};
+
+const client = new Client(
+  vars.PLAID_CLIENT_ID,
+  vars.PLAID_SECRET,
+  vars.PLAID_PUBLIC_KEY,
+  environments[vars.PLAID_ENV],
+  { version: "2018-05-22" }
+);
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.post("/get_access_token", (req, res) => {
+  const { public_token: PUBLIC_TOKEN } = req.body;
+  console.log(PUBLIC_TOKEN);
+  client.exchangePublicToken(PUBLIC_TOKEN, (err, tokenResponse) => {
+    console.log(err, tokenResponse);
+    if (err) {
+      return res.json({ err });
+    }
+
+    const { access_token: ACCESS_TOKEN, item_id: ITEM_ID } = tokenResponse;
+    res.json({
+      access_token: ACCESS_TOKEN,
+      item_id: ITEM_ID,
+      error: null
+    });
+  });
+});
+
+app.listen(8000, () => {
+  console.log("Express server listening on 8000.");
+});
--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Client, environments } from "plaid";
+import PlaidLink from "react-plaid-link";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      PLAID_CLIENT_ID: "5c52345ce341ed0010a522f1",
+      PLAID_SECRET: "259a3db7aec2d3314a6e545d056a10",
+      PLAID_PUBLIC_KEY: "134893e5d974bced3a52c91e8e6b5a",
+      PLAID_ENV: "development",
+      ACCESS_TOKEN: "access-development-24a603b2-2718-4a6e-bf97-51a1b81f3303",
+      ITEM_ID: "J9Bb060X4wTKRj3mbmYJCZkNBP8kgeHbdO3AK"
+    };
+  }
+
+  handleOnSuccess(token, metadata) {
+    console.log("public token:", token);
+    fetch("http://localhost:8000/get_access_token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        public_token: token
+      })
+    })
+      .then(res => res.json())
+      .then(res => {
+        const { ACCESS_TOKEN, ITEM_ID } = res;
+        this.setState({
+          ACCESS_TOKEN,
+          ITEM_ID
+        });
+      });
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    const { PLAID_PUBLIC_KEY, ACCESS_TOKEN } = this.state;
+    return ACCESS_TOKEN ? (
+      <button>GET TRANSACTIONS</button>
+    ) : (
+      <PlaidLink
+        clientName="testApp"
+        env={"development"}
+        product={["transactions"]}
+        publicKey={PLAID_PUBLIC_KEY}
+        onSuccess={this.handleOnSuccess}
+      >
+        Sign on modal
+      </PlaidLink>
     );
   }
 }
